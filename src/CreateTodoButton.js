2888432c6b1b718631defc8fd75ef528ec8441fe
Brayan Rivera Cepeda <riveracepedabrayan@gmail.com>
2022-07-04 22:06:20 -0500

    first components

---
+++ src/CreateTodoButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateTodoButton() {
+  return <button>Add Todo</button>;
+}
+
+export { CreateTodoButton };
---
+++ src/TodoCounter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TodoCounter() {
+  return <h2>You already finished x of y TODOs</h2>;
+}
+
+export { TodoCounter };
